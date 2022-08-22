797b34ad4222eef147de69bdf5368fd53459eda8
rz <muhamaul@gmail.com>
2022-08-22 14:45:12 +0700

    [Clojure] finished chapter 4

--- ch04-concurrency-parallelism/src/ch04_concurrency_parallelism/core.clj
+++ ch04-concurrency-parallelism/src/ch04_concurrency_parallelism/core.clj
@@ -1,6 +1,12 @@
 (ns ch04-concurrency-parallelism.core
+  (:require [clojure.java.io :as io]
+           [net.cgrand.enlive-html :as enlive])
+  (:use [clojure.string :only (lower-case)])
+  (:import [java.net URL MalformedURLException]
+           [java.util.concurrent BlockingQueue LinkedBlockingQueue])
   (:gen-class))
 
+;(require '[net.cgrand.enlive-html :as enlive])
 (defn -main
   "I don't do a whole lot ... yet."
   [& args]
@@ -244,3 +250,128 @@
 (comment
   (send a range 1000)
   (send a1 inc))
+
+(def console (agent *out*))
+(def character-log (agent (io/writer "character-states.log" :append true)))
+
+(defn write
+  [^java.io.Writer w & content]
+  (doseq [x (interpose " " content)]
+    (.write w (str x)))
+  (doto w
+    (.write "\n")
+    .flush))
+
+(defn log-reference
+  [reference & writer-agents]
+  (add-watch reference :log
+             (fn [_ reference old new]
+               (doseq [writer-agent writer-agents]
+                 (send-off writer-agent write new)))))
+
+(log-reference bilbo console character-log)
+(log-reference smaug console character-log)
+
+(defn- links-from
+  [base-url html]
+  (remove nil? (for [link (enlive/select html [:a])]
+                 (when-let [href (-> link :attrs :href)]
+                   (try
+                     (URL. base-url href)
+                     ;; ignore bad urls
+                     (catch MalformedURLException e))))))
+
+(defn- words-from
+  [html]
+  (let [chunks (-> html
+                   (enlive/at [:script] nil)
+                   (enlive/select [:body enlive/text-node]))]
+    (->> chunks
+         (mapcat (partial re-seq #"\w+"))
+         (remove (partial re-matches #"\d+"))
+         (map lower-case))))
+
+(def url-queue (LinkedBlockingQueue.))
+(def crawled-urls (atom #{}))
+(def word-freqs (atom {}))
+
+(declare get-url)
+(def agents (set (repeatedly 25 #(agent {::t #'get-url :queue url-queue}))))
+
+(declare run process handle-results)
+
+(defn ^::blocking get-url
+  [{:keys [^BlockingQueue queue] :as state}]
+  (let [url (io/as-url (.take queue))]
+    (try
+      (if (@crawled-urls url)
+        state
+        {:url url
+         :content (slurp url)
+         ::t #'process})
+      (catch Exception e
+        ;; skip any url we failed to load
+        state)
+      (finally (run *agent*)))))
+
+(defn process
+  [{:keys [url content]}]
+  (try
+    (let [html (enlive/html-resource (java.io.StringReader. content))]
+      {::t #'handle-results
+       :url url
+       :links (links-from url html)
+       :words (reduce (fn [m word]
+                        (update-in m [word] (fnil inc 0)))
+                      {}
+                      (words-from html))})
+    (finally (run *agent*))))
+
+(defn ^::blocking handle-results
+  [{:keys [url links words]}]
+  (try
+    (swap! crawled-urls conj url)
+    (doseq [url links]
+      (.put url-queue url))
+    (swap! word-freqs (partial merge-with +) words)
+    {::t #'get-url :queue url-queue}
+    (finally (run *agent*))))
+
+(defn paused?
+  [agent]
+  (::paused (meta agent)))
+
+(defn run
+  ([] (doseq [a agents] (run a)))
+  ([a]
+   (when (agents a)
+     (send a (fn [{transition ::t :as state}]
+               (when-not (paused? *agent*)
+                 (let [dispatch-fn (if (-> transition meta ::blocking)
+                                     send-off
+                                     send)]
+                   (dispatch-fn *agent* transition)))
+               state)))))
+
+(defn pause
+  ([] (doseq [a agents] (pause a)))
+  ([a] (alter-meta! a assoc ::paused true)))
+
+(defn restart
+  ([] (doseq [a agents] (restart a)))
+  ([a]
+   (alter-meta! a dissoc ::paused)
+   (run a)))
+
+(defn test-crawler
+  [agent-count starting-url]
+  (def agents (set (repeatedly agent-count
+                               #(agent {::t #'get-url :queue url-queue}))))
+  (.clear url-queue)
+  (swap! crawled-urls empty)
+  (swap! word-freqs empty)
+  (.add url-queue starting-url)
+  (run)
+  (Thread/sleep 60000)
+  (pause)
+  [(count @crawled-urls) (count url-queue)])
